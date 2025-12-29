acd814a25e9d9aa053d68028044b3a41c142e2fa
hcppcrlab-crypto <hcppcrlab@gmail.com>
2025-12-29 13:48:49 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// No actual connection code - just simulation
-function simulateConnection() {
-    setTimeout(() => {
-        alert('Simulated Connection Successful!');
-    }, Math.random() * 3000);
-}
-document.querySelector('.connect-btn').addEventListener('click', simulateConnection);
